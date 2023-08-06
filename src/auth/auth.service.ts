58af02c9d10bffa03f9c2d73f628f7b5961c1cd6
hatemBay <bayoudhhatem5@gmail.com>
2023-08-06 18:30:41 +0100

    added more user attributes to payload

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -24,6 +24,7 @@ export class AuthService {
       username: user.username,
       sub: user.id,
       roles: user.roles,
+      ...user,
     };
 
     return {
--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -17,6 +17,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
       id: payload.sub,
       username: payload.username,
       roles: payload.roles,
+      ...payload,
     };
   }
 }
